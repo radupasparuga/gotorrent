12ede3f579366049467b4218efcf72695287de85
radupasparuga <radupasparuga25@gmail.com>
2021-03-22 22:42:50 +0200

    Add: Basic user input + file reading

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	pathPtr := flag.String("path", "", "Path to .torrent file")
+	flag.Parse()
+
+	data, err := ioutil.ReadFile(*pathPtr)
+
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	fmt.Printf("Contents of file: %s\n", string(data))
+}
